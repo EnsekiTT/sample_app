c6588553c3492aa165bc35bab9d2d3137399e4bf
EnsekiTT <ensekitt@gmail.com>
2018-04-21 06:51:48 +0000

    Add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -64,7 +64,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "authenticated? should returh false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 
 end
